2bb46176d7f0a400bdff39377058b130ab9c93b4
Daniel Tkach <danieltkach@gmail.com>
2021-12-26 02:04:07 -0300

    axis

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { useState, useEffect } from 'react';
 import { D3Line } from "./D3Line"
- 
 
-export const App =()=> {
-	const [D3LineData, setD3LineData] = useState([20, 15, 50, 24, 32, 43, 0, 100, 23, 56, 20,
-		20, 15, 50, 24, 32, 43, 0, 100, 23, 56, 20]);
 
-	useEffect(()=>{
-		const timer1 = setInterval(()=>{
+export const App = () => {
+	// const tempLine = [20, 15, 50, 24, 32, 43, 0, 100, 23, 56, 20,	20, 15, 50, 24, 32, 43, 0, 100, 23, 56, 20];
+	// let lines = [tempLine, tempLine, tempLine, tempLine];
+	// const [D3LineData, setD3LineData] = useState(lines);
+	const [D3LineData, setD3LineData] = useState([20, 15, 50, 24, 32, 43, 0, 100, 23, 56, 20,	20, 15, 50, 24, 32, 43, 0, 100, 23, 56, 20]);
+
+	useEffect(() => {
+		const timer1 = setInterval(() => {
 			let newData = [...D3LineData];
 			newData.shift();
-			newData.push(Math.round(Math.random()*101+0));
+			newData.push(Math.round(Math.random() * 100));
 			setD3LineData(newData);
-		}, 500);
 
-		return ()=>clearInterval(timer1);
+			// for (let i = 0; i < 25; i++) {
+			// 	for (let j = 0; j < 4; j++) {
+			// 		lines[j].shift();
+			// 		lines[j].push(Math.round(Math.random() * 100))
+			// 	}
+			// }
+			// setD3LineData(lines);
+		}, 1000);
+
+		return () => clearInterval(timer1);
 	}, [D3LineData]);
 
 	return (
--- src/D3Line.jsx
+++ src/D3Line.jsx
@@ -1,32 +1,59 @@
-import {  useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 export const D3Line = ({ data }) => {
 	const svgRef = useRef();
+	const svgWidth = 800;
+	const svgHeight = 300;
+	const verticalMargin = 40;
+	const horizontalMargin = 40;
+
+	const svg = d3.select(svgRef.current)
+		.attr('width', svgWidth + horizontalMargin)
+		.attr('height', svgHeight + verticalMargin)
+		.style('background', "#eee");
+
+	const xScale = d3.scaleLinear()
+		.domain([0, data.length - 1])
+		.range([0, svgWidth-horizontalMargin]);
+
+	const yScale = d3.scaleLinear()
+		.domain([0, 100])
+		.range([svgHeight-verticalMargin, 0]);
+
+	const line1 = d3.line()
+		.x((v, i) => xScale(i))
+		.y(yScale)
+		.curve(d3.curveCardinal);
+
+	const xAxis = d3.axisBottom(xScale);
+	svg.select('.x-axis')
+		.style('transform', `translate(${horizontalMargin}px, ${svgHeight+10}px)`)
+		.call(xAxis);
+
+	const yAxis = d3.axisLeft(yScale);
+	svg.select('.y-axis')
+	.style('transform', `translate(${horizontalMargin}px, ${verticalMargin}px)`)
+	.call(yAxis);
 
 	useEffect(() => {
-		const svg = d3.select(svgRef.current)
-			.attr('width', 800)
-			.attr('height', 300)
-			.style('background', "#eee")
-
-		const line1 = d3.line()
-			.x((value, index) => index *30)
-			.y(d => 295 - d)
-			.curve(d3.curveCardinal);
-
-
-			svg
-			.selectAll('path')
+		svg
+			.selectAll('.line')
 			.data([data])
 			.join('path')
-			.attr('d', d=>line1(d))
+			.attr('class', 'line')
+			.attr('d', line1)
 			.attr('fill', 'none')
 			.attr('stroke', 'orange')
 			.attr('stroke-width', 3)
+			.style('transform', `translate(${horizontalMargin}px, ${verticalMargin}px)`)
+
 	}, [data]);
 
 	return (
-		<svg ref={svgRef}></svg>
+		<svg ref={svgRef}>
+			<g className="x-axis" />
+			<g className="y-axis" />
+		</svg>
 	)
 }
